e9622ea5ed51d75d21544cd93d5a4ebe10a9661c
tomxpcvx <dev@tomxpcvx.wtf>
2020-09-03 10:56:13 +0200

    🔒 Fixes npm security issues

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
                 loader: "babel-loader",
                 options: {
                     presets: [
-                        'react',
+                        '@babel/preset-react',
                         ['es2015', {
                             modules: false
                         }]
